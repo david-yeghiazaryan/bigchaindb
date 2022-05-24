5fe67d79484f650225716737b6ab96d5c49edfa0
David Yeghiazaryan <David.Yeghiazaryan@synisys.com>
2022-05-24 17:50:13 +0400

    Improvement: Hardcoded and updated version.

--- bigchaindb/version.py
+++ bigchaindb/version.py
@@ -3,13 +3,8 @@
 # SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 # Code is Apache-2.0 and docs are CC-BY-4.0
 
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
+__version__ = '2.2.2.1'
+__short_version__ = '2.2'
 
 # Supported Tendermint versions
 __tm_supported_versions__ = ["0.31.5", "0.22.8"]
